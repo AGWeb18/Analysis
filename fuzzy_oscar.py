99cf1d5583013377fdeef3facebf61e5e8bba892
Anthony Ridding <ARidding@cmcc.ca>
2017-10-30 08:43:48 -0400

    added files to repo

---
+++ fuzzy_oscar.py
@@ -0,0 +1,26 @@
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+import pandas as pd
+
+file = "tracking_codes4.csv"
+fuzzy_df = pd.read_csv(file)
+
+df = fuzzy_df["Event"]
+df.replace("([\W+])","",regex=True, inplace=True)
+
+
+l= []
+
+for i in range(0, len(df)-1):
+    curr_row = df[i]
+    nxt_row = df[i+1]
+    dist = fuzz.ratio(str(curr_row), str(nxt_row))
+    l.append(dist)
+
+l.append(-999)
+fuzzy_df["Distance"] = l
+
+distance_df = fuzzy_df[["Event","Count","Distance"]]
+
+print(distance_df.head())
+#distance_df.to_csv("editdistance_trackingcodes2.csv")
