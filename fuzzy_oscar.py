214cde83cdafa695253ed19a9308cd87f071e3da
Anthony Ridding <ARidding@cmcc.ca>
2017-10-30 15:28:16 -0400

    removed the distance calc - unnecessary

--- fuzzy_oscar.py
+++ fuzzy_oscar.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import Levenshtein
-import re
 
 file = "tracking_codes4.csv"
 fuzzy_df = pd.read_csv(file)
@@ -11,20 +10,10 @@ fuzzy_df["Event"] = df.str.lower()
 fuzzy_df.Event.sort_values()
 fuzzy_df.Event.str.strip()
 
-l= []
 
-for i in range(0, len(df)-1):
-    curr_row = df[i]
-    nxt_row = df[i+1]
-    dist = Levenshtein.distance(str(curr_row), str(nxt_row))
-    l.append(dist)
-
-l.append(-999)
-fuzzy_df["Distance"] = l
-
-distance_df = fuzzy_df[["Event","Count","Distance"]]
+distance_df = fuzzy_df[["Event","Count"]]
 distance_df = distance_df.iloc[:-1,:]
-tracking_codes = ["adj", "mobs", "std","6100","bronte","sherb"]
+tracking_codes = ["adj", "mobs", "std","6100","bronte","sherb","ib"]
 
 def create_df_tracking(df, track_code):
     track_name = str(track_code) + "-frame"
