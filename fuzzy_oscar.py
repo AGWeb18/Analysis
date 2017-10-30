73ffbfb8a3ded7670f9d53d29781080240d3390f
Anthony Ridding <ARidding@cmcc.ca>
2017-10-30 15:22:47 -0400

    improved output of function

--- fuzzy_oscar.py
+++ fuzzy_oscar.py
@@ -24,22 +24,23 @@ fuzzy_df["Distance"] = l
 
 distance_df = fuzzy_df[["Event","Count","Distance"]]
 distance_df = distance_df.iloc[:-1,:]
-tracking_codes = ["adj", "mobs", "6100","bronte"]
+tracking_codes = ["adj", "mobs", "std","6100","bronte","sherb"]
 
 def create_df_tracking(df, track_code):
     track_name = str(track_code) + "-frame"
     track_count = str(track_code) + "-Count"
     track_name = df.Event.str.contains(str(track_code))
     track_count = df[track_name].Count.sum()
-    return ("The count of " + str(track_code) + " is: " + str(track_count))
+    return (df[track_name], "The count of " + str(track_code) + " is: " + str(track_count))
     
 
 
 
 for i in tracking_codes:
     output = create_df_tracking(distance_df, i)
-    print(output)
-
+    print(output[0])
+    print(output[1])
+    print("="*40)
 
 
 #distance_df.to_csv("editdistance_trackingcodes3.csv")
