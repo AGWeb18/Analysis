d821f3b102602c1e3b4aeb42136909ece2b5d550
Anthony Ridding <ARidding@cmcc.ca>
2017-11-10 10:18:14 -0500

    Added GUI

--- fuzzy_oscar.py
+++ fuzzy_oscar.py
@@ -1,46 +1,101 @@
 import pandas as pd
-import Levenshtein
-import re
+import tkinter as tk
+from tkinter import Label
+from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+import os
+import time
+
+wb = Workbook()
+ws = wb.active
 
 file = "tracking_codes4.csv"
 fuzzy_df = pd.read_csv(file)
-
 df = fuzzy_df["Event"]
 df.replace("([\W+])","",regex=True, inplace=True)
+df.str.strip()
+df.drop_duplicates(keep='last')
 fuzzy_df["Event"] = df.str.lower()
-fuzzy_df.Event.sort_values()
-fuzzy_df.Event.str.strip()
+fuzzy_df[["Event", "Count"]]
+fuzzy_df = fuzzy_df.iloc[:,1:]
 
-l= []
 
-for i in range(0, len(df)-1):
-    curr_row = df[i]
-    nxt_row = df[i+1]
-    dist = Levenshtein.distance(str(curr_row), str(nxt_row))
-    l.append(dist)
+    
 
-l.append(-999)
-fuzzy_df["Distance"] = l
-
-distance_df = fuzzy_df[["Event","Count","Distance"]]
-distance_df = distance_df.iloc[:-1,:]
-tracking_codes = ["adj", "mobs", "std","6100","bronte","sherb"]
-
-def create_df_tracking(df, track_code):
-    track_name = str(track_code) + "-frame"
-    track_count = str(track_code) + "-Count"
-    track_name = df.Event.str.contains(str(track_code))
-    track_count = df[track_name].Count.sum()
-    return (df[track_name], "The count of " + str(track_code) + " is: " + str(track_count))
+def create_df_tracking(_df, track_code):
+    track_name = _df.Event.str.contains(str(track_code), na=False)
+    track_count = _df[track_name].Count.sum()
+    return (_df[track_name], "The count of " + str(track_code) + " is: " + str(track_count))
     
 
 
+class CountApp(tk.Frame):
+    ''' An app built to visualize the output of the Oscar code counts.'''
+    def __init__(self, master):
+        tk.Frame.__init__(self,
+                          master,
+                          width=500,
+                          height=500)
+        #  Set the title
+        self.master.title("Oscar Code Count")
 
-for i in tracking_codes:
-    output = create_df_tracking(distance_df, i)
-    print(output[0])
-    print(output[1])
-    print("="*40)
+        #  This allows the size specification to take effect
+        self.pack_propagate(0)
 
+        # We'll use the flexible pack layout manager
+        self.pack()
 
-#distance_df.to_csv("editdistance_trackingcodes3.csv")
+        #   The greeting selector
+        #   Use a StringVar to access the selectors value
+        self.greeting_var = tk.StringVar()
+        self.greeting = tk.OptionMenu(self,
+                                      self.greeting_var,
+                                      "adj", "mobs")
+        self.greeting_var.set('adj')
+
+        #   The receipient text entry control and it's StringVar
+#        self.recipient_var = tk.StringVar()
+#        self.recipient = tk.Entry(self,
+#                                  textvariable=self.recipient_var)
+#        self.recipient_var.set('world')
+
+        #   The go button
+        self.go_button = tk.Button(self, text="Go",
+                                   command=self.printSomething)
+        self.export_button = tk.Button(self, text="Export",
+                                       command=self.exportSomething)
+
+        #  The label
+        self.label = tk.Label(self)
+                                   
+        #   Put the controls on the form
+        self.go_button.pack(fill=tk.X, side=tk.BOTTOM)
+        self.greeting.pack(fill=tk.X, side=tk.TOP)
+        self.export_button.pack(fill=tk.X, side=tk.BOTTOM)
+
+        
+    
+    def printSomething(self):
+        global temp_output
+        temp_output = create_df_tracking(fuzzy_df, self.greeting_var.get())
+        label = Label(text= '%s, \n\n %s' % (temp_output[0], temp_output[1]))
+        #this creates a new label to the GUI
+        label.place(relx=0.5, rely=0.5, anchor='center',bordermode='outside',relheight=0.8, relwidth=0.50)
+
+    def exportSomething(self):
+        temp_df = pd.DataFrame(temp_output[0])
+        #   Append dataframe to xlsx
+        for r in dataframe_to_rows(temp_df, index=False, header=True):
+            ws.append(r)
+        wb.save("CodeCountExport.xlsx")
+        label_export = Label(text="Successfully Exported to XLSX to: "+str(os.getcwd()))
+        label_export.pack(fill = tk.X, side=tk.BOTTOM)
+
+    
+
+    def run(self):
+        ''' Run the app '''
+        self.mainloop()
+
+app = CountApp(tk.Tk())
+app.run()
